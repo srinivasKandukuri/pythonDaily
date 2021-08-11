298bf3a2c0d0bd0d47ac784bc2226b59fa1483d5
~#S*K#~ <srinivasKandukuri@users.noreply.github.com>
2021-08-11 17:32:44 +0530

    Update list_swap.py

--- Basic/list_swap.py
+++ Basic/list_swap.py
@@ -13,3 +13,27 @@ def swapList(list):
 newList = [12, 35, 9, 56, 24]
 
 print(swapList(newList))
+
+
+
+
+
+
+# Python3 program to swap first
+# and last element of a list
+
+# Swap function
+def swapList(list):
+	
+	first = list.pop(0)
+	last = list.pop(-1)
+	
+	list.insert(0, last)
+	list.append(first)
+	
+	return list
+	
+# Driver code
+newList = [12, 35, 9, 56, 24]
+
+print(swapList(newList))
